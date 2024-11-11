aff3e1139afd24503ae820a833c4c33335bc0d21
irinadeeva <deeva97@me.com>
2024-11-11 10:17:38 +0300

    Initial Commit

---
+++ MixedTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MixedTable
+//
+//  Created by Irina Deeva on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
